0718e91ab9fa94c859a754dac77dc2fb6d8b70ed
Rom Burla <romwebdev@gmail.com>
2023-05-24 20:51:53 +0300

    project end

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+export interface User {
+  _id?: string | null,
+  name?: string | null,
+  email?: string | null,
+  password?: string | null,
+  token?: string | null,
+}
+
+export interface Customer{
+  _id?: string|null,
+  firstName?: string|null,
+  lastName?: string|null,
+  phone?: string|null,
+  email?: string|null
+  Address?: string|null
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'frontend';
+}
